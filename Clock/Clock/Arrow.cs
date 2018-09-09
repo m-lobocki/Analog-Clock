1d8f3217d06034d5c4b91417aed1e65749259b0e
m-lobocki <mlobocki@outlook.com>
2018-09-09 19:12:58 +0200

    Encapsulated a line as an arrow; line with extra properties

---
+++ Clock/Clock/Arrow.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Windows.Shapes;
+
+namespace Clock
+{
+    /// <summary>
+    /// Line can only be set once
+    /// </summary>
+    public class Arrow
+    {
+        private Line line;
+
+        public ArrowKind Kind { get; }
+        public double Thickness { get; }
+        public Line Line
+        {
+            get => line;
+            set
+            {
+                if (line is null)
+                {
+                    line = value;
+                }
+                else
+                {
+                    throw new ArgumentException("Line is already set");
+                }
+            }
+        }
+
+        public Arrow(ArrowKind kind, double thickness)
+        {
+            Kind = kind;
+            Thickness = thickness;
+        }
+    }
+}
